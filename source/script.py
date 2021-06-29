844ad0ca67a60f0f4eda2d9d3095bddfeaf7fb5f
amrutsavadatti <amrutsavadatti@gmail.com>
2021-06-29 17:35:08 +0530

    test hello world

--- source/script.py
+++ source/script.py
@@ -1,17 +1,2 @@
-from datetime import datetime
-import pytz
-import time
-
-IST = pytz.timezone('Asia/Kolkata')
-
-
-print("script initialized")
-
 while(True):
-
-  datetime_ist = datetime.now(IST)
-  if datetime_ist.strftime('%H:%M:%S') == "14:30:00" :
-    print("yes")
-    time.sleep(100)
-  else:
-    continue
+  print("heloo world")
