44ead84fabc285d97675c181ee62dea7161e4ecb
amrutsavadatti <amrutsavadatti@gmail.com>
2021-06-29 18:43:51 +0530

    invoke attempt 1

--- source/script.py
+++ source/script.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pytz
 import time
+import requests
 
 IST = pytz.timezone('Asia/Kolkata')
 
@@ -10,8 +11,10 @@ print("script initialized")
 while(True):
 
   datetime_ist = datetime.now(IST)
-  if datetime_ist.strftime('%H:%M:%S') == "14:30:00" :
-    print("yes")
+  if datetime_ist.strftime('%H:%M:%S') == "00:00:00" :
+    print("sending invoke")
+    r =requests.get('http://gopalakmilk.com/Gopalak_Website/Templates/orange_template/now-ui-dashboard-master/examples/deliveryProcess.php?invoke=vacationReimbursement')
+    print(r.status_code)
     time.sleep(100)
   else:
     continue
